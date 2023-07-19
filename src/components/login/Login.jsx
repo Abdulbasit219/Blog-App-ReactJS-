f236d864b898e10268e4d17d09eace71a6e40414
Abdul Basit <abdulbasitbasit076@gmail.com>
2023-07-19 15:56:27 +0500

    first Commit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,22 @@
+import "./Login.css"
+
 function Login () {
 
     return (
-         <div>
-            <p>Login Page</p>
+         <div className="first">
+            <div className="container card p-3 mt-2 login_container">
+               <p className="font">Login Form</p>
+            <form>
+               <div className="form group">
+                  <label htmlFor="">Email</label>
+                  <input type="text" name="" required className="form-control"></input>
+               </div>
+               <div className="form group">
+                  <label htmlFor="">Password</label>
+                  <input type="password" name="" required className="form-control"></input>
+               </div>
+            </form>
+            </div>
          </div>
         )
 }
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,48 +1,73 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Signup.css"
 import { VscEyeClosed, VscEye } from "react-icons/vsc";
 
+function Reg() {
 
-function Reg(props) {
+    const [isregister, setIsregister] = useState({
+        name: null,
+        email: null,
+        password: null,
+        // showpass: false
+    });
 
-    let  passtype, btnicon;
-    if (props.showpassword === true) {
-        btnicon = <VscEyeClosed />;
-        passtype = "text";
-
+    const onchange = (e) => {
+        const { value, name } = e.target
+        setIsregister(() => {
+            return {
+                ...isregister,
+                [name]: value
+            }
+        })
     }
-    else {
-        passtype = "password";
-        btnicon = <VscEye />;
-    }
 
+    // const showpasshandle = () => {
+    //  setIsregister({showpass: !isregister.showpass}
+    // )}
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const { name, email, password } = isregister;
+        if( name===null || email===null || password === null ){ 
+            alert("All fields are required");
+        }
+    // let  passtype, btnicon;
+    // if (isregister.showpass === true) {
+    //     btnicon = <VscEyeClosed />;
+    //     passtype = "text";
+
+    // }
+    // else {
+    //     passtype = "password";
+    //     btnicon = <VscEye />;
+    // }
 
     return (
         <div className="first">
             <div className="container card p-3 mt-2 reg_container">
                 <p className="font">Registeration Form</p>
 
-                <form onSubmit={props.submit}>
+                <form>
 
                     <div className="form-group">
                         <label htmlFor="name">Name</label>
-                        <input type="text" name="name" required className="form-control"></input>
+                        <input type="text" name="name" onChange={onchange} className="form-control"></input>
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" required className="form-control"></input>
+                        <input type="text" name="email" onChange={onchange} className="form-control"></input>
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="passwords">Password</label>
+                        <label htmlFor="password">Password</label>
                         <div>
-                            <input type={passtype} name="passwords" required className="form-control"></input>
-                            <button className="btn" type="button" onClick={props.showpasshandle}>{btnicon}</button>
+                            <input type="password" name="password" onChange={onchange} className="form-control"></input>
+                            {/* <button className="btn" type="button" onClick={showpasshandle}>{btnicon}</button> */}
                         </div>
                     </div>
 
-                    <button class="btn btn-primary" type="submit">Register</button>
+                    <button class="btn btn-primary" onClick={onSubmit}>Register</button>
                 </form>
             </div>
         </div>
@@ -50,4 +75,4 @@ function Reg(props) {
 
 }
 
-export default Reg
+export default Reg;
